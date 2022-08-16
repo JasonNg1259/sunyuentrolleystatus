c2eb208f52344ffa967c0fd0ebc21a13c1795934
JasonNg1259 <110475679+JasonNg1259@users.noreply.github.com>
2022-08-17 01:04:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,16 @@
     <body>
         <header>
             <?php
-                include_once "php/config.php";
+                //Get Heroku ClearDB connection information
+                $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+                $cleardb_server = $cleardb_url["host"];
+                $cleardb_username = $cleardb_url["user"];
+                $cleardb_password = $cleardb_url["pass"];
+                $cleardb_db = substr($cleardb_url["path"],1);
+                $active_group = 'default';
+                $query_builder = TRUE;
+                // Connect to DB
+                $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
             ?>
         </header>
         <h1 style="text-decoration: underline; text-align: center;">Extrusion Trolley</h1>
